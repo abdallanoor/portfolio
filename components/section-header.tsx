0f2bf3d0ccc65b29d7f716644fe6a98fa8cd034b
Abdallah <abdallaahnoor@gmail.com>
2025-09-12 21:51:16 +0300

    fix: add width to section header container for improved layout

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -7,21 +7,23 @@ export default function SectionHeader({
 }) {
   return (
     <>
-      <div className="relative inline-block text-foreground">
+      <div className="relative inline-block w-fit text-foreground">
         <h2 className="text-3xl font-light">{title}</h2>
-        <svg
-          className="w-full h-2 rtl:mt-2"
-          viewBox="0 0 200 20"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M10,15 Q100,5 190,15"
-            stroke="currentColor"
-            strokeWidth="3"
-            fill="none"
-            strokeLinecap="round"
-          />
-        </svg>
+        <div className="w-full">
+          <svg
+            className="w-full max-w-80 h-2 rtl:mt-2"
+            viewBox="0 0 200 20"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M10,15 Q100,5 190,15"
+              stroke="currentColor"
+              strokeWidth="3"
+              fill="none"
+              strokeLinecap="round"
+            />
+          </svg>
+        </div>
       </div>
       <p className="mt-4 font-light opacity-80">{description}</p>
     </>
