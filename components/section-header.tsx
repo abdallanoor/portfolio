339d575f766f1484da3f845541b0106f1a82670d
Abdallah <abdallaahnoor@gmail.com>
2025-09-13 17:45:30 +0300

    fix: adjust layout of section header for improved visual consistency

--- components/section-header.tsx
+++ components/section-header.tsx
@@ -7,25 +7,23 @@ export default function SectionHeader({
 }) {
   return (
     <>
-      <div className="relative inline-block w-fit text-foreground">
+      <div className="relative block max-w-fit text-foreground">
         <h2 className="text-3xl font-light">{title}</h2>
-        <div className="w-full">
-          <svg
-            className="w-full max-w-80 h-2 rtl:mt-2"
-            viewBox="0 0 200 20"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M10,15 Q100,5 190,15"
-              stroke="currentColor"
-              strokeWidth="3"
-              fill="none"
-              strokeLinecap="round"
-            />
-          </svg>
-        </div>
+        <svg
+          className="absolute -bottom-2 rtl:-bottom-4 left-0 w-full h-2"
+          viewBox="0 0 200 20"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M10,15 Q100,5 190,15"
+            stroke="currentColor"
+            strokeWidth="3"
+            fill="none"
+            strokeLinecap="round"
+          />
+        </svg>
       </div>
-      <p className="mt-4 font-light opacity-80">{description}</p>
+      <p className="mt-8 font-light opacity-80">{description}</p>
     </>
   );
 }
